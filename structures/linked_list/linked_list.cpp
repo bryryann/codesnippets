a3e0d5d1c0ccf214c47ec386adaac023765dc0ae
bryryann <186111776+bryryann@users.noreply.github.com>
2026-06-22 18:00:43 -0300

    finish linked list implementation

--- structures/linked_list/linked_list.cpp
+++ structures/linked_list/linked_list.cpp
@@ -8,22 +8,36 @@ struct Node {
 
 struct List {
     int count;
+    int next_key;
     Node *init;
 };
 
 void create_list(List *l) {
     l->count = 0;
+    l->next_key = 0;
     l->init = nullptr;
+}
+
+void limpar_lista(List *list) {
+    Node *aux = list->init;
+
+    while (aux != nullptr) {
+        Node *prox = aux->next;
+        delete aux;
+        aux = prox;
+    }
+
+    list->init = nullptr;
+    list->count = 0;
+    list->next_key = 0;
 }
 
 void push_back(List *list, int value) {
     Node *new_node = new Node;
 
-    if (new_node != nullptr) {
-        new_node->value = value;
-        new_node->key = list->count;
-        new_node->next = nullptr;
-    }
+    new_node->value = value;
+    new_node->key = ++list->next_key;
+    new_node->next = nullptr;
 
     if (list->init == nullptr) {
         list->init = new_node;
@@ -39,5 +53,42 @@ void push_back(List *list, int value) {
     }
 
     list->count++;
+}
+
+void push_front(List *list, int value) {
+    Node *new_node = new Node;
+
+    new_node->value = value;
+    new_node->key = ++list->next_key;
+    new_node->next = list->init;
+
+    list->init = new_node;
+    list->count++;
+}
+
+Node* pop_back(List *list) {
+    Node *node;
+
+    if (list->init == nullptr) {
+        return nullptr;
+    }
+
+    node = list->init;
+
+    if (list->init->next == nullptr) {
+        list->init = nullptr;
+        list->count--;
+        return node;
+    }
+
+    Node *aux = nullptr;
+    while (node->next != nullptr) {
+        aux = node;
+        node = node->next;
+    }
+
+    list->count--;
+    aux->next = nullptr;
+    return node;
 }
 
